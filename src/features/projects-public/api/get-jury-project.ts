81b5d1054f63c42120029fd1f76b26c9bd0631b1
yzhuye <yzhu@uninorte.edu.co>
2025-11-27 01:50:52 -0500

    feat(integration): Jury project

--- src/features/projects-public/api/get-jury-project.ts
+++ src/features/projects-public/api/get-jury-project.ts
@@ -4,28 +4,44 @@ import { api } from "@/lib/api-client";
 import { QueryConfig } from "@/lib/react-query";
 import { Meta, Project } from "@/types/api";
 
-export const getJuryProjects = (
-  { page, eventId }: { page?: number; eventId?: string } = { page: 1 }
+export const getJuryProjects = async (
+  { page, jurorId, eventId}: { page?: number; jurorId?: string; eventId?: number } = { page: 1 }
 ): Promise<{ data: Project[]; meta: Meta }> => {
-  return api.get(`/events/${eventId}/projects`, { params: { page } });
+  const response = await api.get<{
+      items: Project[];
+      page: number;
+      limit: number;
+      total: number;
+      totalPages: number;
+    }>(`/projects/assigned-projects`, { params: { page, jurorId, eventId } });
+
+  return {
+    data: response.items || [],
+    meta: {
+      page: response.page,
+      total: response.total,
+      totalPages: response.totalPages,
+    },
+  };
 };
 
-export const getJuryProjectsQueryOptions = ({ page = 1, eventId }: { page?: number; eventId?: string } = {}) => {
+export const getJuryProjectsQueryOptions = ({ page = 1, jurorId , eventId }: { page?: number; jurorId?: string; eventId?: number } = {}) => {
   return queryOptions({
-    queryKey: ["projects", { page, eventId }],
-    queryFn: () => getJuryProjects({ page, eventId }),
+    queryKey: ["projects", { page, jurorId, eventId }],
+    queryFn: () => getJuryProjects({ page, jurorId, eventId }),
   });
 };
 
 type UseProjectsOptions = {
   page?: number;
-  eventId?: string;
+  jurorId?: string;
+  eventId?: number;
   queryConfig?: QueryConfig<typeof getJuryProjectsQueryOptions>;
 };
 
-export const useJuryProjects = ({ queryConfig, page, eventId }: UseProjectsOptions) => {
+export const useJuryProjects = ({ queryConfig, page, jurorId, eventId }: UseProjectsOptions) => {
   return useQuery({
-    ...getJuryProjectsQueryOptions({ page, eventId }),
+    ...getJuryProjectsQueryOptions({ page, jurorId, eventId }),
     ...queryConfig,
   });
 };
